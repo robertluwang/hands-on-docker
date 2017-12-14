3d37100081c58604203dd586e8d4e297733caf92
Robert Wang <robert.lu.wang@gmail.com>
2017-12-14 12:26:24 -0500

    Create minikubefw.sh

---
+++ minikubefw.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# minikubefw.sh
+# Robert Wang https://github.com/robertluwang
+# remedy for minikube in VPN/firewall on win
+# Dec 14, 2017
+
+check8443=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 8443|cut -d, -f4|cut -d= -f2`
+if [ $check8443 == '8443' ]; then 
+    # if 127.0.0.1 8443 forward exist, skip
+    echo 
+else 
+    VBoxManage controlvm minikube natpf1 k8s-apiserver,tcp,127.0.0.1,8443,,8443
+fi 
+
+check30000=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 30000|cut -d, -f4|cut -d= -f2`
+if [ $check30000 == '30000' ]; then 
+    # if 127.0.0.1 30000 forward exist, skip
+    echo 
+else 
+    VBoxManage controlvm minikube natpf1 k8s-dashboard,tcp,127.0.0.1,30000,,30000
+fi 
+
+kubectl config set-cluster minikube-vpn --server=https://127.0.0.1:8443 --insecure-skip-tls-verify
+kubectl config set-context minikube-vpn --cluster=minikube-vpn --user=minikube
+kubectl config use-context minikube-vpn  
