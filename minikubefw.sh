c417243ea1e0cd42c91bef9b2741f5d11d8325cf
Robert Wang <robert.lu.wang@gmail.com>
2017-12-15 10:45:02 -0500

    Update minikubefw.sh

--- minikubefw.sh
+++ minikubefw.sh
@@ -3,7 +3,7 @@
 # Robert Wang https://github.com/robertluwang
 # remedy for minikube without TLS verification when firewall ON
 # running platform: win msys64/cygwin64
-# Dec 14, 2017
+# Dec 15, 2017
 
 check8443=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 8443|cut -d, -f4|cut -d= -f2`
 if [ $check8443 == '8443' ]; then 
@@ -11,7 +11,15 @@ if [ $check8443 == '8443' ]; then
     echo 
 else 
     VBoxManage controlvm minikube natpf1 k8s-apiserver,tcp,127.0.0.1,8443,,8443
-fi 
+fi
+
+check2374=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 2374|cut -d, -f4|cut -d= -f2`
+if [ $check2374 == '2374' ]; then 
+    # if 127.0.0.1 2374 forward exist, skip
+    echo 
+else 
+    VBoxManage controlvm minikube natpf1 k8s-docker,tcp,127.0.0.1,2374,,2376
+fi
 
 check30000=`VBoxManage showvminfo minikube|grep 127.0.0.1|grep 30000|cut -d, -f4|cut -d= -f2`
 if [ $check30000 == '30000' ]; then 
@@ -24,3 +32,10 @@ fi
 kubectl config set-cluster minikube-vpn --server=https://127.0.0.1:8443 --insecure-skip-tls-verify
 kubectl config set-context minikube-vpn --cluster=minikube-vpn --user=minikube
 kubectl config use-context minikube-vpn  
+
+eval $(minikube docker-env)
+unset DOCKER_TLS_VERIFY
+export DOCKER_HOST="tcp://127.0.0.1:2374"
+alias docker='docker --tls'
+
+
