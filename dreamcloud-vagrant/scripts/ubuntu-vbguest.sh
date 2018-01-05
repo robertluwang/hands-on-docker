8647785feb197bbad3d82db829e4f0505933877c
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 12:47:06 -0500

    Create ubuntu-vbguest.sh

---
+++ dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# ubuntu base box virtualbox guest tool script for packer
+# Robert Wang @github.com/robertluwang
+# Jan 5th, 2018
+
+apt-get install -y -q gcc build-essential
+
+curl -LO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
+mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
+sh -E /mnt/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_5.2.4.iso
+umount /mnt
