078a1413d9c7cc87429b02db6fdd39f38fd9f7cd
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 12:46:29 -0500

    Create ubuntu-publickey.sh

---
+++ dreamcloud-vagrant/scripts/ubuntu-publickey.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# ubuntu base box public key script for packer
+# Robert Wang @github.com/robertluwang
+# Jan 5th, 2018
+
+
+# reset public key 
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+curl -Lo /home/vagrant/.ssh/authorized_keys  https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+sed -i 's/^#AuthorizedKeysFile/AuthorizedKeysFile/'  /etc/ssh/sshd_config
+
+systemctl restart sshd.service
