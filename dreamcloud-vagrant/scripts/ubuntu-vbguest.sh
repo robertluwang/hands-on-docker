753c81aa7fb670df18263c146e5a7ceb28ed217d
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 13:03:06 -0500

    Update ubuntu-vbguest.sh

--- dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
+++ dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
@@ -5,7 +5,7 @@
 
 apt-get install -y -q gcc build-essential
 
-curl -LO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
+curl -sSLO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
 mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
 sh -E /mnt/VBoxLinuxAdditions.run
 rm VBoxGuestAdditions_5.2.4.iso
