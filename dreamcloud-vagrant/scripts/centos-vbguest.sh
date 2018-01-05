f2001d57fcf6d86a11dd155e4316f49f1397c2fc
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 13:02:32 -0500

    Update centos-vbguest.sh

--- dreamcloud-vagrant/scripts/centos-vbguest.sh
+++ dreamcloud-vagrant/scripts/centos-vbguest.sh
@@ -5,7 +5,7 @@
 
 yum install -y -q gcc make kernel-devel bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r` 
 
-curl -LO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
+curl -sSLO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
 mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
 sh -E /mnt/VBoxLinuxAdditions.run
 rm VBoxGuestAdditions_5.2.4.iso
