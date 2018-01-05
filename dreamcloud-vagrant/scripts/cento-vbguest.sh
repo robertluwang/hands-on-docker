b9b6126587f77c45cb8aea74c3f02e99ecd8b081
Robert Wang <robert.lu.wang@gmail.com>
2018-01-04 22:59:43 -0500

    Create cento-vbguest.sh

---
+++ dreamcloud-vagrant/scripts/cento-vbguest.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# centos base box virtualbox guest tool script for packer
+# Robert Wang @github.com/robertluwang
+# Jan 4th, 2018
+
+yum install -y -q gcc make bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r` 
+
+curl -LO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
+mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
+sh -E /mnt/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_5.2.4.iso
+umount /mnt
+
+yum remove -y -q gcc make bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r`
+
