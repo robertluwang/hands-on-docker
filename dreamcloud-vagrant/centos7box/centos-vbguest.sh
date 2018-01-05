eaf5ebb77e9f8525c11402bb8489546b812ca3f9
Robert Wang <robert.lu.wang@gmail.com>
2018-01-04 22:36:57 -0500

    Update centos-vbguest.sh

--- dreamcloud-vagrant/centos7box/centos-vbguest.sh
+++ dreamcloud-vagrant/centos7box/centos-vbguest.sh
@@ -3,10 +3,12 @@
 # Robert Wang @github.com/robertluwang
 # Jan 4th, 2018
 
-yum install -y -q dkms gcc make kernel-devel bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r` 
+yum install -y -q gcc make bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r` 
 
 curl -LO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
 mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
 sh -E /mnt/VBoxLinuxAdditions.run
 rm VBoxGuestAdditions_5.2.4.iso
 umount /mnt
+
+yum remove -y -q gcc make bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers  kernel-devel-`uname -r`
