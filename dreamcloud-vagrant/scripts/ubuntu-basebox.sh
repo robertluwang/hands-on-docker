4fd4ae5436b86d2512c62b9e97194c3d268992a0
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 12:44:40 -0500

    Create ubuntu-basebox.sh

---
+++ dreamcloud-vagrant/scripts/ubuntu-basebox.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#  ubuntu base box provision script for packer  
+#  Robert Wang
+#  Jan 4th, 2018
+
+# add user vagrant to sudo group vagrant 
+groupadd vagrant
+usermod -a -G vagrant vagrant
+
+mv /etc/sudoers.d/vagrant  /etc/sudoers.d/vagrant.old
+echo '%vagrant ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+
+# update system
+apt-get update -y -q && apt-get upgrade -y -q 
+apt-get install -y -q python git dos2unix wget ifupdown 
