6a1d6407a220098d9b543df3bf97a6f5bdd292fd
Robert Wang <robert.lu.wang@gmail.com>
2018-01-05 21:00:54 -0500

    Update ubuntu-vbguest.sh

--- dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
+++ dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
@@ -3,10 +3,15 @@
 # Robert Wang @github.com/robertluwang
 # Jan 5th, 2018
 
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -y -q purge virtualbox*
 apt-get install -y -q gcc build-essential
 
 curl -sSLO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
 mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
-sh -E /mnt/VBoxLinuxAdditions.run
+
+sh -E /mnt/VBoxLinuxAdditions.run /S
 rm VBoxGuestAdditions_5.2.4.iso
 umount /mnt
+
