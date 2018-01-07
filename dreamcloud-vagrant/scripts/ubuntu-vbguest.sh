e823c381a4fe1430d70775ff04d7a0e39dfa5d9a
Robert Wang <robert.lu.wang@gmail.com>
2018-01-06 21:57:44 -0500

    Update ubuntu-vbguest.sh

--- dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
+++ dreamcloud-vagrant/scripts/ubuntu-vbguest.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 # ubuntu base box virtualbox guest tool script for packer
 # Robert Wang @github.com/robertluwang
-# Jan 5th, 2018
+# Jan 6th, 2018
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y -q purge virtualbox*
 apt-get install -y -q gcc build-essential
 
-curl -sSLO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
-mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
-
-sh -E /mnt/VBoxLinuxAdditions.run /S
-rm VBoxGuestAdditions_5.2.4.iso
+# you can download guest tool iso 
+#curl -sSLO http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
+#mount -o loop,ro VBoxGuestAdditions_5.2.4.iso /mnt
+# packer will upload guest tool iso to home
+mount -o loop,ro VBoxGuestAdditions.iso /mnt
+yes | sh /mnt/VBoxLinuxAdditions.run || echo "VBoxLinuxAdditions.run exited $? and is suppressed."
+rm VBoxGuestAdditions.iso
 umount /mnt
+
+
 
