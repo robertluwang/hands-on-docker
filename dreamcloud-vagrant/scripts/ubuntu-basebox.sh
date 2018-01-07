3d81c8888dec1d74aa114a7c6def6dad6ac37f86
Robert Wang <robert.lu.wang@gmail.com>
2018-01-06 21:55:52 -0500

    Update ubuntu-basebox.sh

--- dreamcloud-vagrant/scripts/ubuntu-basebox.sh
+++ dreamcloud-vagrant/scripts/ubuntu-basebox.sh
@@ -6,13 +6,22 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # add user vagrant to sudo group vagrant 
-groupadd vagrant
+if [ ! `cat /etc/group | grep vagrant`];then
+    groupadd vagrant
+fi
+
 usermod -a -G vagrant vagrant
 
-mv /etc/sudoers.d/vagrant  /etc/sudoers.d/vagrant.old
+if [ -d /etc/sudoers.d/vagrant ];then
+    rm /etc/sudoers.d/vagrant  
+fi 
+
+touch /etc/sudoers.d/vagrant
+
 echo '%vagrant ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
 
 # update system
 apt-get update -y -q && apt-get upgrade -y -q 
 apt-get install -y -q python git dos2unix wget ifupdown 
+
