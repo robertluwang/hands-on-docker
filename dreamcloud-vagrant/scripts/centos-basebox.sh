152d899b8703a9206c75e775b8b263ba658a67e1
Robert Wang <robert.lu.wang@gmail.com>
2018-01-06 22:24:50 -0500

    Update centos-basebox.sh

--- dreamcloud-vagrant/scripts/centos-basebox.sh
+++ dreamcloud-vagrant/scripts/centos-basebox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #  centos  base  box  provision  script for packer  
 #  Robert Wang
-#  Jan 4th, 2018
+#  Jan 6th, 2018
 
 # fix  primary  NAT  interface issue
 ifcfgname=`ls /etc/sysconfig/network-scripts|grep ifcfg|grep -v ifcfg-lo|sort|head -1`
@@ -9,10 +9,18 @@ sed -i '/ONBOOT=no/ s/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/$ifc
 sed -i '/UUID/d' /etc/sysconfig/network-scripts/$ifcfgname
 
 # add user vagrant to sudo group vagrant 
-groupadd vagrant
+if [ ! `cat /etc/group | grep vagrant`];then
+    groupadd vagrant
+fi
+
 usermod -a -G vagrant vagrant
 
-mv /etc/sudoers.d/vagrant  /etc/sudoers.d/vagrant.old
+if [ -d /etc/sudoers.d/vagrant ];then
+    rm /etc/sudoers.d/vagrant  
+fi 
+
+touch /etc/sudoers.d/vagrant
+
 echo '%vagrant ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
 
@@ -22,3 +30,9 @@ sed -i -e 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
 # update system
 yum -y -q update && sudo yum -y -q upgrade 
 yum install -y -q git dos2unix wget
+
+
+
+
+
+
