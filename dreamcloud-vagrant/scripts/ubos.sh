549610c00f82d9821d30e74ff1ac1d113f16547a
Robert Wang <robert.lu.wang@gmail.com>
2018-01-22 18:57:32 -0500

    Create ubos.sh

---
+++ dreamcloud-vagrant/scripts/ubos.sh
@@ -0,0 +1,19 @@
+
+
+sudo apt install bridge-utils
+
+git clone https://git.openstack.org/openstack-dev/devstack
+cd devstack
+
+# prepare local.conf
+cat <<EOF > local.conf
+[[local|localrc]] 
+ADMIN_PASSWORD=secret
+DATABASE_PASSWORD=\$ADMIN_PASSWORD
+RABBIT_PASSWORD=\$ADMIN_PASSWORD
+SERVICE_PASSWORD=\$ADMIN_PASSWORD
+HOST_IP_IFACE=enp0s8
+EOF
+
+# run devstack
+./stack.sh
