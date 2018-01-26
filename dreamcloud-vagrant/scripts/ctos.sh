21f14395f9323bf2eb5a62beeea40b108b81500e
Robert Wang <robert.lu.wang@gmail.com>
2018-01-26 17:36:36 -0500

    Update ctos.sh

--- dreamcloud-vagrant/scripts/ctos.sh
+++ dreamcloud-vagrant/scripts/ctos.sh
@@ -12,7 +12,7 @@ systemctl enable network
 systemctl start network
 
 # step 1 sw repo
-yum install -y centos-release-openstack-pike
+yum install -y centos-release-openstack-pike openstack-utils
 yum-config-manager --enable openstack-pike
 yum update -y
 
@@ -21,5 +21,12 @@ yum install -y openstack-packstack
 
 # step3 run packstack
 packstack --gen-answer-file=packstack_`date +"%Y-%m-%d"`.conf
-sed -i 's/10.0.2.15/10.120.0.21/g' packstack_`date +"%Y-%m-%d"`.conf
+
+sed -i '/CONFIG_DEFAULT_PASSWORD=/c CONFIG_DEFAULT_PASSWORD=demo' packstack_`date +"%Y-%m-%d"`.conf
+sed -i '/CONFIG_KEYSTONE_ADMIN_PW=/c CONFIG_KEYSTONE_ADMIN_PW=demo' packstack_`date +"%Y-%m-%d"`.conf
+sed -i '/CONFIG_KEYSTONE_DEMO_PW=/c CONFIG_KEYSTONE_DEMO_PW=demo' packstack_`date +"%Y-%m-%d"`.conf
+sed -i '/CONFIG_SWIFT_INSTALL=/c CONFIG_SWIFT_INSTALL=n' packstack_`date +"%Y-%m-%d"`.conf
+ 
+sed -i '/10.0.2.15/s/10.0.2.15/10.120.0.21/g' packstack_`date +"%Y-%m-%d"`.conf
+
 packstack --answer-file packstack_`date +"%Y-%m-%d"`.conf
