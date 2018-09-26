1ce110e5ae3a55bfad194ddadb63c5cee28a4348
Robert Wang <robert.lu.wang@gmail.com>
2018-09-26 06:27:55 -0400

    Create rhel7.3-cleanup.sh

---
+++ dreamcloud-vagrant/scripts/rhel7.3-cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# rhel7.3 base box cleanup script for packer
+# Robert Wang @github.com/robertluwang
+# Sept 22th, 2018
+
+#  remove vbox guest source 
+rm -rf /usr/src/vboxguest* 
+
+# remove under tmp directory
+rm -rf /tmp/*
+
+# remove caches
+find /var/cache -type f -exec rm -rf {} \;
+
+# delete any logs that have built up during the install
+find /var/log/ -name *.log -exec rm -f {} \;
+
+# defrag space 
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
+rm -f /EMPTY
+
+# clean history
+cat /dev/null > ~/.bash_history && history -c
